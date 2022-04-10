c18a3bfc072acd3ac851da9713764272700ad1f6
Katelin Burke <katelinburke@gmail.com>
2022-04-10 11:59:22 -0600

    deliverable 1

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,15 @@
+# Deliberable 1: Linear Regression
+
 library(dplyr)
 
-car_df <- read.csv(MechaCar_mpg.csv, header = TRUE, sep = ',')
+car_df <- read.csv('MechaCar_mpg.csv', header = TRUE, sep = ',')
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=car_df)
+summary (lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=car_df))
+
+# Deliverable 2: Trip Analysis Visualizations
+
+suspension_df <- read.csv('Suspension_Coil.csv', header=TRUE, sep = ',')
+total_summary <- suspension_df %>% 
+  # I don't understand what I'm supposed to be grouping by
+  
+# Deliverable 3:
